6e86bc9fc4055bedecb64aab0c6d234e38722fa3
amedama41 <kamo.devel41@gmail.com>
2024-05-20 00:13:02 +0900

    refactor: include scallop edit lines into completion

--- lua/cmp_shell_history/init.lua
+++ lua/cmp_shell_history/init.lua
@@ -4,6 +4,19 @@ local default_option = {
   history_filepath = "~/.bash_history",
 }
 
+---@param items { label: string, kind: number }[]
+---@param lines string[]
+---@param duplicated table<string, boolean>
+local function to_item(items, lines, duplicated)
+  for i = #lines, 1, -1 do
+    local line = lines[i]
+    if line and not duplicated[line] then
+      duplicated[line] = true
+      items[#items + 1] = { label = line, kind = 1 }
+    end
+  end
+end
+
 function source.new()
   return setmetatable({}, { __index = source })
 end
@@ -16,32 +29,29 @@ function source:complete(params, callback)
   local option = vim.tbl_extend("keep", params.option, default_option)
   local history_filepath = vim.fn.expand(option.history_filepath)
 
+  local buffer_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  local items = {}
+  local duplicated = {}
+  to_item(items, buffer_lines, duplicated)
+
   local fd = vim.loop.fs_open(history_filepath, "r", 0)
   if fd == nil then
-    return callback()
+    return callback({ items = items })
   end
 
   local stat = vim.loop.fs_fstat(fd)
   if stat == nil then
     vim.loop.fs_close(stat)
-    return callback()
+    return callback{ items = items }()
   end
 
   vim.loop.fs_read(fd, stat.size, nil, function(err, data)
     vim.loop.fs_close(fd)
     if err ~= nil then
-      callback()
+      callback{ items = items }()
     end
     local lines = vim.split(data, "\n", { plain = true })
-    local items = {}
-    local duplicated = {}
-    for i = #lines, 1, -1 do
-      local line = lines[i]
-      if line and not duplicated[line] then
-        duplicated[line] = true
-        items[#items + 1] = { label = line, kind = 1 }
-      end
-    end
+    to_item(items, lines, duplicated)
     callback({ items = items })
   end)
 end
